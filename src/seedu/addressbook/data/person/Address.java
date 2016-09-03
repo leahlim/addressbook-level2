9eae384ee35e60c4457c7ed50f453a5262ae8aaf
Leah Lim <leah_lim@hotmail.com>
2016-09-03 17:59:21 +0800

    changing add address format

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,12 +8,66 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String EXAMPLE = "a/123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses has to be in this format: a/BLOCK, STREET, UNIT, POSTAL_CODE";
+    public static final String ADDRESS_VALIDATION_REGEX = "(.+),(.+),(.+),(.+)";
+    public static final int ADDRESS_SIZE = 4;
 
-    public final String value;
+    //public final String value;
     private boolean isPrivate;
+    
+    private Block block;
+    private Street street;
+    private Unit unit;
+    private PostalCode postalCode;
+    
+    class Block {
+        private String block;
+        
+        public Block(String block) {
+            this.block = block;
+        }
+        
+        public String getBlock() {
+            return block;
+        }
+    }
+    
+    class Street {
+        private String street;
+        
+        public Street(String street) {
+            this.street = street;
+        }
+        
+        public String getStreet() {
+            return street;
+        }
+    }
+    
+    class Unit {
+        private String unit;
+        
+        public Unit(String unit) {
+            this.unit = unit;
+        }
+        
+        public String getUnit() {
+            return unit;
+        }
+    }
+    
+    class PostalCode {
+        private String postalCode;
+        
+        public PostalCode(String postalCode) {
+            this.postalCode = postalCode;
+        }
+        
+        public String getPostalCode() {
+            return postalCode;
+        }
+    }
 
     /**
      * Validates given address.
@@ -22,10 +76,19 @@ public class Address {
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         this.isPrivate = isPrivate;
-        if (!isValidAddress(address)) {
+        String[] addressParts = splitAddress(address);
+        if (!isValidAddress(address) || addressParts.length != ADDRESS_SIZE) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = address;
+        //this.value = address;
+        block = new Block(addressParts[0]);
+        street = new Street(addressParts[1]);
+        unit = new Unit(addressParts[2]);
+        postalCode = new PostalCode(addressParts[3]);
+    }
+    
+    public String[] splitAddress(String address) {
+        return address.split(", ");
     }
 
     /**
@@ -37,19 +100,19 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return block.getBlock() + ", " + street.getStreet() + ", " + unit.getUnit() + ", " + postalCode.getPostalCode();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.toString().equals(((Address) other).toString())); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return toString().hashCode();
     }
 
     public boolean isPrivate() {
